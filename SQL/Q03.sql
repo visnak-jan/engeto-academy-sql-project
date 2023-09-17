94fb23157754e3b14058451204037d9d3401aa1a
visnak-jan <71195156+visnak-jan@users.noreply.github.com>
2023-09-17 17:58:11 +0200

    Update Q03.sql
    Změna názvů tabulek

--- SQL/Q03.sql
+++ SQL/Q03.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE t_zmena_potravin
+CREATE OR REPLACE TABLE t_food_differences
 SELECT rok, product_name, average_price,
 	round(((average_price - (lag(average_price) OVER (PARTITION BY product_name  ORDER BY rok))) * 100) / (lag(average_price) OVER (PARTITION BY product_name  ORDER BY rok)), 2) AS percent_change
 FROM t_jan_visnak_project_sql_primary_final
@@ -7,19 +7,19 @@ ORDER BY product_name, rok;
 
 
 
-SELECT * FROM t_zmena_potravin tzp ;
+SELECT * FROM t_food_differences ;
 
 
 SELECT t1.rok, t2.product_name, t1.min_difference_percent
 FROM (
 	SELECT rok, min(percent_change) AS min_difference_percent
-	FROM t_zmena_potravin
+	FROM t_food_differences
 	WHERE percent_change IS NOT NULL
 	GROUP BY rok
 	ORDER BY rok) t1
 JOIN (
 	SELECT *
-	FROM t_zmena_potravin) t2
+	FROM t_food_differences) t2
 ON t1.min_difference_percent = t2.percent_change
 ORDER BY t1.rok;
 
@@ -28,28 +28,28 @@ ORDER BY t1.rok;
  SELECT t1.rok, t1.product_name, t1.average_price, t2.average_price, round(((t2.average_price / t1.average_price) * 100 ) - 100, 2) AS percent_change
 FROM (
 	SELECT rok, product_name, average_price 
-	FROM t_zmena_potravin
+	FROM t_food_differences
 	WHERE rok = 2006) t1
 JOIN (
 	SELECT rok, product_name, average_price
-	FROM t_zmena_potravin
+	FROM t_food_differences
 	WHERE rok = 2018) t2
 ON t1.product_name = t2.product_name;
 
 
-SELECT t1.industry_name, t1.rok, t1.average_value, t2.rok, t2.average_value ,
-	t2.average_value - t1.average_value AS value_increase,
-	round(((t2.average_value-t1.average_value) * 100)/ t1.average_value,2) AS percent_increase
+SELECT wd1.industry_name, wd1.rok, wd1.average_value, wd2.rok, wd2.average_value ,
+	wd2.average_value - wd1.average_value AS value_increase,
+	round(((wd2.average_value-wd1.average_value) * 100)/ wd1.average_value,2) AS percent_increase
 FROM (
 	SELECT rok, industry_name, average_value
-	FROM t_mzdy_rozdily
+	FROM t_wages_differences
 	WHERE rok = (
 	SELECT min(rok) 
-	FROM t_mzdy_rozdily)) t1
+	FROM t_wages_differences)) wd1
 JOIN (
 	SELECT rok, industry_name, average_value
-	FROM t_mzdy_rozdily
+	FROM t_wages_differences
 	WHERE rok = (
 	SELECT max(rok)
-	FROM t_mzdy_rozdily)) t2
-ON t1.industry_name = t2.industry_name;
+	FROM t_wages_differences)) wd2
+ON wd1.industry_name = wd2.industry_name;

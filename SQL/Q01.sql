ea01cfad5b53fa6347fa4d4cf34509410b61f611
visnak-jan <71195156+visnak-jan@users.noreply.github.com>
2023-09-17 17:57:30 +0200

    Update Q01.sql
    Změna názvů tabulek

--- SQL/Q01.sql
+++ SQL/Q01.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE t_mzdy_rozdily
+CREATE OR REPLACE TABLE t_wages_differences
 SELECT rok, industry_code,  industry_name, average_value,
 	average_value - (lag(average_value) OVER (PARTITION BY industry_name ORDER BY rok)) AS "změna",
 	round(((average_value - (lag(average_value) OVER (PARTITION BY industry_name ORDER BY rok))) * 100) / (lag(average_value) OVER (PARTITION BY industry_name ORDER BY rok)), 2) AS percent_change,
@@ -14,7 +14,7 @@ ORDER BY industry_name;
 
 # počet pokledů
 SELECT rok, count(industry_code) AS 'počet'
-FROM t_mzdy_rozdily
+FROM t_wages_differences
  WHERE difference_comment = 'pokles'
 GROUP BY rok;
 
@@ -23,7 +23,7 @@ sELECT cpib.name
 FROM czechia_payroll_industry_branch cpib
 left JOIN (
 	SELECT industry_name 
-	FROM t_mzdy_rozdily
+	FROM t_wages_differences
 	WHERE difference_comment  IN ('pokles')
 	GROUP BY industry_name) t1
 ON cpib.name = t1.industry_name
@@ -32,23 +32,23 @@ WHERE t1.industry_name IS NULL;
 
 # chybí další otázky!!!!
 
-SELECT t1.industry_name, t1.rok, t1.average_value, t2.rok, t2.average_value ,
-	t2.average_value - t1.average_value AS value_increase,
-	round(((t2.average_value-t1.average_value) * 100)/ t1.average_value,2) AS percent_increase
+SELECT wd1.industry_name, wd1.rok, wd1.average_value, wd2.rok, wd2.average_value ,
+	wd2.average_value - wd1.average_value AS value_increase,
+	round(((wd2.average_value-wd1.average_value) * 100)/ wd1.average_value,2) AS percent_increase
 FROM (
 	SELECT rok, industry_name, average_value
-	FROM t_mzdy_rozdily
+	FROM t_wages_differences
 	WHERE rok = (
 	SELECT min(rok) 
-	FROM t_mzdy_rozdily)) t1
+	FROM t_wages_differences)) wd1
 JOIN (
 	SELECT rok, industry_name, average_value
-	FROM t_mzdy_rozdily
+	FROM t_wages_differences
 	WHERE rok = (
 	SELECT max(rok)
-	FROM t_mzdy_rozdily)) t2
-ON t1.industry_name = t2.industry_name;
+	FROM t_wages_differences)) wd2
+ON wd1.industry_name = wd2.industry_name;
 
 
 
-SELECT * FROM t_mzdy_rozdily;
+SELECT * FROM t_wages_differences;
